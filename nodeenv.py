2f4e7a0b897b6210149d040fb80b5dee4e4ae727
Vincent Bernat <bernat@luffy.cx>
2014-02-18 22:14:45 +0100

    Fix location of npm install.sh.
    The new location is `www.npmjs.org/install.sh`. curl is not configured
    to follow a redirection.

--- nodeenv.py
+++ nodeenv.py
@@ -406,7 +406,7 @@ def install_npm(env_dir, src_dir, opt):
         '. %s && curl --silent %s | '
         'clean=%s npm_install=%s bash && deactivate_node' % (
             pipes.quote(join(env_dir, 'bin', 'activate')),
-            'https://npmjs.org/install.sh',
+            'https://www.npmjs.org/install.sh',
             'no' if opt.no_npm_clean else 'yes',
             opt.npm
         )

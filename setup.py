0ef25780942c9b301d3e90344e013b3fc7e2af22
Eugene Kalinin <e.v.kalinin@gmail.com>
2016-05-16 11:22:55 +0300

    setup.py: added classifiers for python 2.6/2.7, fixed #164

--- setup.py
+++ setup.py
@@ -46,6 +46,8 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 )

36312be84d88a8b556d22778cfdcd1f23251d110
Eugene Kalinin <e.v.kalinin@gmail.com>
2022-06-25 18:33:28 +0300

    1.7.0

--- nodeenv.py
+++ nodeenv.py
@@ -47,7 +47,7 @@ except ImportError:  # pragma: no cover (py3 only)
 
 from pkg_resources import parse_version
 
-nodeenv_version = '1.6.0'
+nodeenv_version = '1.7.0'
 
 join = os.path.join
 abspath = os.path.abspath
